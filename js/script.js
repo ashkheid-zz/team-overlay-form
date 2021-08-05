b0299d34790459c34d3738dfd3dad7f2a17ac493
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-08-05 19:09:11 +0430

    Trying to make email validation work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const regEx = `^(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$
+`;
+const emailInput = document.getElementById("email");
+
+
+function test() {
+    console.log("Hi TEST!!!!!");
+    // emailInput.pattern = regEx;
+}
+
+window.onload = test();
+
+/*
 function validateForm() {
     var fieldName = document.forms["form"]["name"].value;
     var fieldPhone = document.forms["form"]["phone"].value;
@@ -29,4 +42,4 @@ function hide() {
 function show() {
     document.getElementById("form").style.display = "flex";
 
-}
+}*/
