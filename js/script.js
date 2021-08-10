9c4ad682a0fa18838370aa0755217c3273705d50
hamed <hmd366nasiri@gmail.com>
2021-08-10 20:39:43 +0430

    create menu and title for first page

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
-const regEx = `^(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$
-`;
-const emailInput = document.getElementById("email");
+// const regEx = `^(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$
+// `;
+// const emailInput = document.getElementById("email");
 
 
-function test() {
-    console.log("Hi TEST!!!!!");
-    // emailInput.pattern = regEx;
-}
-
-
-// function emailValidate(form, email) {
-//     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-//     var address = document.forms[form].elements[email].value;
-//     if (reg.test(address) == false) {
-//         alert('آدرس ایمیل وارد شده نامعتبر است');
-//         return false;
-//     }
+// function test() {
+//     console.log("Hi TEST!!!!!");
+//     // emailInput.pattern = regEx;
 // }
 
 
-window.onload = test();
+// window.onload = test();
 
 
 function hide() {
@@ -27,15 +17,17 @@ function hide() {
     document.getElementById("form").style.display = "none";
 
 
+
+
 }
 
 
 
 
-// function show() {
-//     document.getElementById("form").style.display = "flex";
+function show() {
+    document.getElementById("form").style.display = "block";
 
-// }
+}
 
 
 
@@ -102,7 +94,12 @@ function validateForm(string) {
         alert("فرم ارسال نشد");
     } else if (check == 1) {
         document.getElementById("form").remove();
-        alert("با تشکر فرم ارسال شد");
+        swal({
+            title: "فرم شما با موفقیت ارسال شد",
+            text: "لطفاْ بر روی دکمه تایید کلیک کنید",
+            icon: "success",
+            button: "تایید",
+        });
 
     }
 
@@ -132,3 +129,21 @@ document.addEventListener('keyup', function(e) {
 
     }
 });
+
+
+// function showAndHide() {
+
+//     if (document.getElementById("form").onclick == 0) {
+//         alert("hello");
+//     } else {
+//         document.getElementById("form").style.display = "none";
+
+//     }
+// }
+// document.body.addEventListener("click", my);
+
+
+// function my() {
+
+//     document.getElementById("form").style.display = "none";
+// }
