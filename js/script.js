b15529523d0c02a57ae40a244077e0d68e7625bb
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2021-08-12 17:35:44 +0430

    Phone validation added

--- js/script.js
+++ js/script.js
@@ -49,6 +49,27 @@ function validateForm(string) {
     }
   }
 
+  //this funcation validates the phone number
+  function validatePhoneNumber(input_str) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    return re.test(input_str);
+  }
+
+  function validateForm(event) {
+    var phone = document.getElementById("phone").value;
+    if (!validatePhoneNumber(phone)) {
+      document.getElementById("phone_error").classList.remove("hidden");
+    } else {
+      document.getElementById("phone_error").classList.add("hidden");
+      alert("validation success");
+    }
+    event.preventDefault();
+  }
+
+  document.getElementById("form").addEventListener("submit", validateForm);
+  //phone number validation ends
+
   var fieldName = (document.forms["form"]["fullname"].value = "");
   var fieldPhone = (document.forms["form"]["phone"].value = "");
   var fieldPhone = (document.forms["form"]["email"].value = "");
